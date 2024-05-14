d2a1c0f30af2038c8bec8fb4149fdd01a6a38949
Rosalinda Cedillo <rosalindacedillo2017@gmail.com>
2024-05-14 13:45:03 -0600

    mejore

--- ArrayCalculationApp/Form1.cs
+++ ArrayCalculationApp/Form1.cs
@@ -1,28 +1,42 @@
+
 namespace ArrayCalculationApp
 {
     public partial class Form1 : Form
     {
-
         private int[] numbers;
+        private int[,] matrix;
+        private int[,,] cube;
         private int index;
 
         public Form1()
         {
             InitializeComponent();
-            numbers = new int[5]; 
+            numbers = new int[5];
+            matrix = new int[3, 3]; // Matriz 3x3
+            cube = new int[3, 3, 3]; // Cubo 3x3x3
             index = 0;
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
+            // Tu código actual para agregar números a 'numbers'...
+
+            // Agregar números a 'matrix' y 'cube'
             try
             {
                 if (index < numbers.Length)
                 {
-
                     numbers[index++] = int.Parse(txtNumber.Text);
                     lblNumbers.Text += txtNumber.Text + ", ";
                     txtNumber.Clear();
+
+                    // Agregar números a la matriz 3x3
+                    // Por simplicidad, supongamos que 'index' se usa para agregar números a cada elemento de la matriz
+                    matrix[index / 3, index % 3] = numbers[index - 1];
+
+                    // Agregar números al cubo 3x3x3
+                    // De manera similar, supongamos que 'index' se usa para agregar números a cada elemento del cubo
+                    cube[index / 9, (index / 3) % 3, index % 3] = numbers[index - 1];
                 }
                 else
                 {
@@ -41,18 +55,34 @@ namespace ArrayCalculationApp
 
         private void btnCalculate_Click(object sender, EventArgs e)
         {
-            
-            double sum = 0;
-            foreach (int number in numbers)
-            {
-                sum += number;
-            }
-            double average = sum / numbers.Length;
+            // Tu código actual para calcular el promedio de 'numbers'...
 
-            // Mostrar el resultado en un cuadro de diálogo
-            MessageBox.Show("The average of the numbers entered is: " + average.ToString("F2"), "Result");
+            // Calcular suma de todos los elementos en la matriz 3x3
+            int sumMatrix = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    sumMatrix += matrix[i, j];
+                }
+            }
+
+            // Calcular suma de todos los elementos en el cubo 3x3x3
+            int sumCube = 0;
+            for (int i = 0; i < cube.GetLength(0); i++)
+            {
+                for (int j = 0; j < cube.GetLength(1); j++)
+                {
+                    for (int k = 0; k < cube.GetLength(2); k++)
+                    {
+                        sumCube += cube[i, j, k];
+                    }
+                }
+            }
+
+            // Aquí puedes mostrar las sumas de la matriz y el cubo en un MessageBox o donde prefieras
+            MessageBox.Show("Sum of elements in the matrix: " + sumMatrix + "\nSum of elements in the cube: " + sumCube, "Result");
+
         }
     }
- 
 }
-    
